b3cdd166b4dad8b8d0496256d35518313c342e6d
mssavai <mssavai@gmail.com>
2013-08-08 20:43:00 +0000

    Second Update to web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs=require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var str=fs.readFileSync('index.html','utf8');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
